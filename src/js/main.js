6c5a7e57461b1ff75688c0df1bb453620ba70612
trb2021 <mazzito2015@gmail.com>
2022-06-03 14:18:11 -0400

    Casi terminada secctión 'Coming soon' solo falta descripción e imágenes

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,27 @@ var swiper = new Swiper(".home", {
       clickable: true,
     }
   });
+
+  var swiper = new Swiper(".coming-container", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+      delay:55000,
+      disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+      },
+      568: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      968: {
+        slidesPerView: 5,
+      },
+    }
+  });
