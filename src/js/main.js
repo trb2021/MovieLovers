db47c4ddd4ad10d6960ee6d633374ff580a6ffcc
Tomas Rodriguez <mazzito2015@gmail.com>
2022-06-05 22:06:14 -0400

    Terminada la maquetación de la página principal

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,19 @@ window.addEventListener('scroll', ()=>{
   header.classList.toggle('shadow', window.scrollY > 0);
 });
 
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+}
+
 var swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
